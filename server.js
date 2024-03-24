44f88b2e695d0e7cfbdc5a90ce1471b309641361
Shivam Tiwari <shivam.tiwari@consectus.com>
2024-03-24 10:35:50 +0530

    Mongodb connection established

--- server.js
+++ server.js
@@ -26,8 +26,9 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use('/api', router);
 app.use('/admin', admin);
-
-
+app.get('/',function(req,res){
+  console.log("Your project is runnng");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
